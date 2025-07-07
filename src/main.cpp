6522944504e659ef8b4f12c38c20e2f1af93ab5b
Gordon Ross <gordon.ross88@gmail.com>
2025-07-06 19:39:32 -0700

    Added compile option for using RGB WS2812b LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#define USE_RGB_LED 1
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <FastLED.h>
 
 #define LED_PIN                 2
 #define MOTION_INPUT_PIN        4
@@ -22,12 +25,17 @@ void LedSet(bool ledIsOn);
 void SetupWifi(void);
 void SerialRxHandle(void);
 void ProcessSerialMessage(char* pMsg, uint8_t msgLen);
+void SetColorSettings(const char* pMsg, uint8_t msgLen);
+bool ValidateAndExtractHsvFromString(const char* pMsg, uint8_t msgLen, CRGB* ledRgb, uint8_t numLeds);
 void MatchStringAndControlPin(const char* pMsg, uint8_t msgLen, const char* pString, uint8_t pin, uint8_t val);
 uint32_t RunningTimeMs(void);
+void ActivateLed(uint8_t pin, uint8_t val);
 
 typedef struct
 {
   unsigned long powerOnTimeMs;
+  CRGB ledRgbCurrent;
+  CRGB ledRgbSettings;
 }G;
 static G g;
 
@@ -46,22 +54,26 @@ void setup(void)
 void SetupVars(void)
 {
   g.powerOnTimeMs = millis();
+  g.ledRgbSettings = CHSV(0, 255, 200);
 }
 void SetupPins(void)
 {
   pinMode(LED_PIN, OUTPUT);
   LedSet(false);
 
-
   pinMode(PC_IN_USE_OUTPUT_PIN, OUTPUT);
   pinMode(MOTION_OUTPUT_PIN, OUTPUT);
+#if USE_RGB_LED
+  FastLED.addLeds<NEOPIXEL, TEAMS_ACTIVE_OUTPUT_PIN>(&g.ledRgbCurrent, 1);
+#else
   pinMode(TEAMS_ACTIVE_OUTPUT_PIN, OUTPUT);
+#endif
   pinMode(DOOR_CLOSED_OUTPUT_PIN, OUTPUT);
 
-  digitalWrite(PC_IN_USE_OUTPUT_PIN, LOW);
-  digitalWrite(MOTION_OUTPUT_PIN, LOW);
-  digitalWrite(TEAMS_ACTIVE_OUTPUT_PIN, LOW);
-  digitalWrite(DOOR_CLOSED_OUTPUT_PIN, LOW);
+  ActivateLed(PC_IN_USE_OUTPUT_PIN, LOW);
+  ActivateLed(MOTION_OUTPUT_PIN, LOW);
+  ActivateLed(TEAMS_ACTIVE_OUTPUT_PIN, LOW);
+  ActivateLed(DOOR_CLOSED_OUTPUT_PIN, LOW);
 
   pinMode(MOTION_INPUT_PIN, INPUT);
   pinMode(DOOR_CLOSED_INPUT_PIN, INPUT);
@@ -81,8 +93,8 @@ void LedSet(bool on)
 void loop(void)
 {
   SerialRxHandle();
-  digitalWrite(MOTION_OUTPUT_PIN, digitalRead(MOTION_INPUT_PIN));
-  digitalWrite(DOOR_CLOSED_OUTPUT_PIN, digitalRead(DOOR_CLOSED_INPUT_PIN));
+  ActivateLed(MOTION_OUTPUT_PIN, digitalRead(MOTION_INPUT_PIN));
+  ActivateLed(DOOR_CLOSED_OUTPUT_PIN, digitalRead(DOOR_CLOSED_INPUT_PIN));
 }
 void SerialRxHandle(void) 
 {
@@ -138,20 +150,129 @@ void SerialRxHandle(void)
 }
 void ProcessSerialMessage(char* pMsg, uint8_t msgLen)
 {
+  SetColorSettings(pMsg, msgLen);
   MatchStringAndControlPin(pMsg, msgLen, "workstation locked", PC_IN_USE_OUTPUT_PIN, LOW);
   MatchStringAndControlPin(pMsg, msgLen, "user is active", PC_IN_USE_OUTPUT_PIN, HIGH);
   MatchStringAndControlPin(pMsg, msgLen, "teams call started", TEAMS_ACTIVE_OUTPUT_PIN, HIGH);
   MatchStringAndControlPin(pMsg, msgLen, "no teams use", TEAMS_ACTIVE_OUTPUT_PIN, LOW);
 }
+void SetColorSettings(const char* pMsg, uint8_t msgLen)
+{
+  const char* pString = "set color settings: ";
+  uint8_t len = strlen(pString);
+  if (memcmp(pMsg, pString, len) == 0)
+  {
+    if (ValidateAndExtractHsvFromString(pMsg + len, msgLen - len, &g.ledRgbSettings, 1))
+    {
+      Serial.println(pMsg);
+    }
+  }
+}
+bool ValidateAndExtractHsvFromString(const char* pMsg, uint8_t msgLen, CRGB* ledRgb, uint8_t numLeds)
+{
+  const char* pMsgOriginal = pMsg;
+  bool expectingNumber = true;
+  uint8_t numberCount = 0;
+  uint8_t spaceCount = 0;
+  while (msgLen--)
+  {
+    if (expectingNumber)
+    {
+      if (isdigit(*pMsg))
+      {
+        pMsg++;
+        numberCount++;
+        expectingNumber = false;
+        continue;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    if (isdigit(*pMsg))
+    {
+      pMsg++;
+    }
+    else if (isspace(*pMsg))
+    {
+      pMsg++;
+      spaceCount++;
+      expectingNumber = true;
+    }
+  }
+  
+  if ((numberCount != numLeds*3) || (spaceCount != (numLeds*3 - 1)))
+  {
+    return false;
+  }
+
+  pMsg = pMsgOriginal;
+  for (uint8_t numIdx = 0; numIdx < numberCount; numIdx++)
+  {
+    int32_t number = atoi(pMsg);
+    if (number > 255)
+    {
+      return false;
+    }
+    if (numIdx < spaceCount)
+    {
+      pMsg = strchr(pMsg, ' ') + 1;
+    }
+  }
+
+  pMsg = pMsgOriginal;
+  for (uint8_t ledIdx = 0; ledIdx < numLeds; ledIdx++)
+  {
+    uint32_t hue = atoi(pMsg);
+    pMsg = strchr(pMsg, ' ') + 1;
+    uint32_t saturation = atoi(pMsg);
+    pMsg = strchr(pMsg, ' ') + 1;
+    uint32_t value = atoi(pMsg);
+
+    ledRgb[ledIdx] = CHSV(hue, saturation, value);
+
+    if ((ledIdx + 1) < numLeds)
+    {
+      pMsg = strchr(pMsg, ' ') + 1;
+    }
+  }
+
+  return true;
+}
 void MatchStringAndControlPin(const char* pMsg, uint8_t msgLen, const char* pString, uint8_t pin, uint8_t val)
 {
   if (msgLen == strlen(pString) && memcmp(pMsg, pString, msgLen) == 0)
   {
-    digitalWrite(pin, val);
+    ActivateLed(pin, val);
     Serial.println(pString);
   }
 }
 uint32_t RunningTimeMs(void)
 {
   return millis() - g.powerOnTimeMs;
+}
+void ActivateLed(uint8_t pin, uint8_t val)
+{
+#if USE_RGB_LED
+  if (pin == TEAMS_ACTIVE_OUTPUT_PIN)
+  {
+    if (val == HIGH)
+    {
+      g.ledRgbCurrent = g.ledRgbSettings;
+    }
+    else
+    {
+      g.ledRgbCurrent = CRGB(0, 0, 0);
+    }
+    FastLED.show();
+  }
+  else
+  {
+    digitalWrite(pin, val);
+  }
+#else
+  digitalWrite(pin, val);
+#endif
 }
